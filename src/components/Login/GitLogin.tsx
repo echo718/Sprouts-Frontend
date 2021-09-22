a02663fb73aeea5337d50f7011fff4da32f8ef91
echo718 <tong.sun@whitireianz.ac.nz>
2021-09-23 10:54:06 +1200

    latest

--- src/components/Login/GitLogin.tsx
+++ src/components/Login/GitLogin.tsx
@@ -2,7 +2,7 @@ import unnamed from '../../assets/unnamed.jpg';
 import KidProfile from './KidProfile';
 import { Login_AccessToken } from '../../apis/apis';
 import { useMutation } from '@apollo/client';
-import { useState, useEffect,useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import React from 'react';
 
 //get code and token.
@@ -14,48 +14,55 @@ export default function GitLogin() {
     const [accessToken] = useMutation(Login_AccessToken)
     const [kidId, setKidId] = useState()
 
+    const getToken =  useCallback(
+        async    () => {
+            await   accessToken({ variables: { code: githubCode } }).then(r => {
+                if (r.errors) {
+                    console.log("findtoken2", githubCode)
+                    let err = r.errors.join("\n");
+                    console.log(err)
+                    alert("Remote Server Error! Please try to login again.")
+                    return
+                }
+                if (r) {
+                    console.log("findtoken3", githubCode)
+                    setKidId(r.data.login.kid.id)
+
+                    localStorage.setItem("Token", r.data.login.jwt)
+                    localStorage.setItem("kidId", r.data.login.kid.id)
+                    localStorage.setItem("gitHub", r.data.login.kid.gitHub)
+
+                    isGetSelfInfo ? setIsGetSelfInfo(false) : setIsGetSelfInfo(true)
+
+                    //if get token, reload page to make githubcode is null. thus, could avoid one senario:
+                    //login, url with code from OAuth, click logout button directly. githubcode give value to localstorage.code, and cause fake logout. When go back to login page, will show logout button again.
+                    // window.location.href="https://sproutsfrontend.azurewebsites.net/Gitlogin"
+                    window.location.href = "http://localhost:3000/Gitlogin"
+                }
+            }).catch(reason => {
+                console.log("findtoken4", githubCode)
+                console.log(reason)
+            })
+            console.log("gettoken")
+        }, [githubCode, isGetSelfInfo, accessToken]
+    )
+
     //get token 
     const findtoken = useCallback(
         () => {
-            console.log("findtoken", githubCode,isGetSelfInfo)
-           
+            console.log("findtoken", githubCode, isGetSelfInfo)
+
             //if code exist and haven't get self kid information, will execute blow code.
             if (githubCode && (!isGetSelfInfo === true)) {
                 //open & close "personal information" bar
                 setIsGetSelfInfo(!isGetSelfInfo)
                 console.log("findtoken1", githubCode)
                 //get token from backend
-                accessToken({ variables: { code: githubCode } }).then(r => {
-                    if (r.errors) {
-                        console.log("findtoken2", githubCode)
-                        let err = r.errors.join("\n");
-                        console.log(err)
-                        alert("Remote Server Error! Please try to login again.")
-                        return
-                    }
-                    if (r) {
-                        console.log("findtoken3", githubCode)
-                        setKidId(r.data.login.kid.id)
-    
-                        localStorage.setItem("Token", r.data.login.jwt)
-                        localStorage.setItem("kidId", r.data.login.kid.id)
-                        localStorage.setItem("gitHub", r.data.login.kid.gitHub)
-    
-                        isGetSelfInfo ? setIsGetSelfInfo(false) : setIsGetSelfInfo(true)
-                        
-                        //if get token, reload page to make githubcode is null. thus, could avoid one senario:
-                        //login, url with code from OAuth, click logout button directly. githubcode give value to localstorage.code, and cause fake logout. When go back to login page, will show logout button again.
-                         window.location.href="https://sproutsfrontend.azurewebsites.net/Gitlogin"
-                         //window.location.href = "http://localhost:3000/Gitlogin"    
-                    }
-                }).catch(reason => {
-                    console.log("findtoken4", githubCode)
-                    console.log(reason)
-                })
+                getToken()
             }
-            },
-        [githubCode,isGetSelfInfo,accessToken],
-    )          
+        },
+        [githubCode, isGetSelfInfo, getToken],
+    )
 
     useEffect(() => {
         console.log("here go to useeffect")
@@ -71,67 +78,69 @@ export default function GitLogin() {
             console.log("useEffect token")
             return;
         }
-       findtoken()
-        console.log("Useeffect",githubCode,isGetSelfInfo,window.localStorage.getItem("code"),window.localStorage.getItem("Token"))
-    }
+        findtoken()
+        console.log("Useeffect", githubCode, isGetSelfInfo, window.localStorage.getItem("code"), window.localStorage.getItem("Token"))
+    }, [githubCode, findtoken, isGetSelfInfo]
     )
 
     const Login = () => {
         const client_id = '519bd96d57a3139af825';
         const authorize_uri = 'https://github.com/login/oauth/authorize';
-        const redirect_uri = 'https://sproutsfrontend.azurewebsites.net/Gitlogin';
-        //const redirect_uri='http://localhost:3000/Gitlogin'    
+        //const redirect_uri = 'https://sproutsfrontend.azurewebsites.net/Gitlogin';
+        const redirect_uri = 'http://localhost:3000/Gitlogin'
         window.location.href = `${authorize_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}`;
-       // setIsGetSelfInfo(!isGetSelfInfo)
+        // setIsGetSelfInfo(!isGetSelfInfo)
     }
 
     const loginStyle = { backgroundImage: `url(${unnamed})`, width: "100%", height: "200px", paddingTop: "5%", textAlign: "center" } as const
 
     return (
+
         <div className="container" style={{ minHeight: '30rem' }}>
-            {/* if code exist and hasnot log out, go to access token. */}
-            {console.log("render",isGetSelfInfo)}
-          
             {
-                githubCode && (window.localStorage.getItem("Token") !== '-1') && isGetSelfInfo ?
-                    findtoken() : ''
+                <div>
+                    {/* if code exist and hasnot log out, go to access token. */}
+                    {console.log("render", isGetSelfInfo)}
+
+                    {
+                        githubCode && (window.localStorage.getItem("Token") !== '-1') && isGetSelfInfo ?
+                            findtoken() : ''
+                    }
+
+                    {/* if localstorage donot have code,and code is not -1, githubcode will store value to loalstorage. */}
+                    {
+                        githubCode ?
+                            (
+                                githubCode !== window.localStorage.getItem("code") ?
+                                    window.localStorage.setItem("code", githubCode) : ''
+                            ) : ''
+                    }
+
+                    {/* if code exist, show kid profile, otherwise, show login button;
+if not click logout button when closed chrome last time, will show kidprofile too*/}
+                    {
+                        (githubCode ? githubCode : window.localStorage.getItem("code"))
+                            && (window.localStorage.getItem("Token") !== '-1') && true
+                            ?
+                            //show kid profile
+                            <div>
+                                <KidProfile isGetSelfInfo={isGetSelfInfo} kidId={kidId} code={githubCode} />
+                            </div>
+                            :
+                            //if login not successful, not show kid profile, show login btn
+                            <div style={{ marginTop: "10%", padding: "0 30%" }}>
+                                <div style={loginStyle}>
+                                    <h2>Welcome</h2><br />
+                                    <button className="btn btn-outline-primary" onClick={Login} >Login with GitHub</button>
+                                    {window.localStorage.setItem("Token", '')}
+                                </div>
+                            </div>
+                    }
+                </div>
             }
 
-            {/* if localstorage donot have code,and code is not -1, githubcode will store value to loalstorage. */}
-            {
-                githubCode ?
-                    (
-                        githubCode !== window.localStorage.getItem("code") ?
-                            window.localStorage.setItem("code", githubCode) : ''
-                    ) : ''
-            }
-
-            {/* if logout, isGetSelfInfo is false, thus, will not execute fintToken() */}
-            {/* {
-               ( window.localStorage.getItem("Token") === '-1') ? setIsGetSelfInfo(false) : ''
-            } */}
-
-            {/* if code exist, show kid profile, otherwise, show login button;
-                if not click logout button when closed chrome last time, will show kidprofile too*/}
-            {
-                (githubCode ? githubCode : window.localStorage.getItem("code"))
-                && (window.localStorage.getItem("Token") !== '-1') &&true
-                    ?
-                    //show kid profile
-                    <div>
-                        <KidProfile isGetSelfInfo={isGetSelfInfo} kidId={kidId} code={githubCode} />
-                    </div>
-                    :
-                    //if login not successful, not show kid profile, show login btn
-                    <div style={{ marginTop: "10%", padding: "0 30%" }}>
-                        <div style={loginStyle}>
-                            <h2>Welcome</h2><br />
-                            <button className="btn btn-outline-primary" onClick={Login} >Login with GitHub</button>
-                            {window.localStorage.setItem("Token", '')}
-                        </div>
-                    </div>
-            }
         </div>
+
 
     )
 }
